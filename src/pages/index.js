a5813e142e51497aeaa9ea5141c9e3e85065d8df
王杨芃 <wangyangpeng@nongfenqi.com>
2018-05-09 13:54:38 +0800

    构建（*）：项目初始化构建init

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Index extends React.Component {
+    render() {
+        return (
+            <h1>douban demo</h1>
+        );
+    }
+}
+
+export default Index;
